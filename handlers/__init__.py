9c81ff3a923d612c8a825373abc40dc475860970
oldPeter616 <oldPeter616@gmail.com>
2025-09-15 20:49:40 +0800

    update: 更新mixin模式

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,11 @@
-from .player_handler import PlayerHandler
-from .shop_handler import ShopHandler
-from .sect_handler import SectHandler
-from .combat_handler import CombatHandler
-from .realm_handler import RealmHandler
-from .misc_handler import MiscHandler
+from .player_handler import PlayerHandlerMixin
+from .shop_handler import ShopHandlerMixin
+from .sect_handler import SectHandlerMixin
+from .combat_handler import CombatHandlerMixin
+from .realm_handler import RealmHandlerMixin
+from .misc_handler import MiscHandlerMixin
 
 __all__ = [
-    "PlayerHandler", "ShopHandler", "SectHandler", 
-    "CombatHandler", "RealmHandler", "MiscHandler"
+    "PlayerHandlerMixin", "ShopHandlerMixin", "SectHandlerMixin", 
+    "CombatHandlerMixin", "RealmHandlerMixin", "MiscHandlerMixin"
 ]
