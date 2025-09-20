19a13f56985bb6a77494766011fd9f6bd0345a40
Zhalslar <zsl2936169201@gmail.com>
2025-09-20 19:38:51 +0800

    fix: 使desc正确显示

--- main.py
+++ main.py
@@ -75,60 +75,60 @@ class XiuXianPlugin(Star):
         logger.info("修仙插件已优雅关闭")
 
     # Misc Commands
-    @filter.command(config.CMD_HELP, "显示帮助信息")
+    @filter.command(config.CMD_HELP, desc="显示帮助信息")
     async def handle_help(self, event: AstrMessageEvent):
         async for r in self.misc_handler.handle_help(event):
             yield r
 
     # Player Commands
-    @filter.command(config.CMD_START_XIUXIAN, "开始你的修仙之路")
+    @filter.command(config.CMD_START_XIUXIAN, desc="开始你的修仙之路")
     async def handle_start_xiuxian(self, event: AstrMessageEvent):
         async for r in self.player_handler.handle_start_xiuxian(event):
             yield r
 
-    @filter.command(config.CMD_PLAYER_INFO, "查看你的角色信息")
+    @filter.command(config.CMD_PLAYER_INFO, desc="查看你的角色信息")
     @player_required
     async def handle_player_info(self, event: AstrMessageEvent, player=None):
         async for r in self.player_handler.handle_player_info(event, player):  # type: ignore
             yield r
 
-    @filter.command(config.CMD_CHECK_IN, "每日签到领取奖励")
+    @filter.command(config.CMD_CHECK_IN, desc="每日签到领取奖励")
     # @player_required
     async def handle_check_in(self, event: AstrMessageEvent, player=None):
         async for r in self.player_handler.handle_check_in(event, player):  # type: ignore
             yield r
 
-    @filter.command(config.CMD_START_CULTIVATION, "开始闭关修炼")
+    @filter.command(config.CMD_START_CULTIVATION, desc="开始闭关修炼")
     @player_required
     async def handle_start_cultivation(self, event: AstrMessageEvent, player=None):
         async for r in self.player_handler.handle_start_cultivation(event, player):  # type: ignore
             yield r
 
-    @filter.command(config.CMD_END_CULTIVATION, "结束闭关修炼")
+    @filter.command(config.CMD_END_CULTIVATION, desc="结束闭关修炼")
     @player_required
     async def handle_end_cultivation(self, event: AstrMessageEvent, player=None):
         async for r in self.player_handler.handle_end_cultivation(event, player):  # type: ignore
             yield r
 
-    @filter.command(config.CMD_BREAKTHROUGH, "尝试突破当前境界")
+    @filter.command(config.CMD_BREAKTHROUGH, desc="尝试突破当前境界")
     @player_required
     async def handle_breakthrough(self, event: AstrMessageEvent, player=None):
         async for r in self.player_handler.handle_breakthrough(event, player):  # type: ignore
             yield r
 
     # Shop Commands
-    @filter.command(config.CMD_SHOP, "查看坊市商品")
+    @filter.command(config.CMD_SHOP, desc="查看坊市商品")
     async def handle_shop(self, event: AstrMessageEvent):
         async for r in self.shop_handler.handle_shop(event):
             yield r
 
-    @filter.command(config.CMD_BACKPACK, "查看你的背包")
+    @filter.command(config.CMD_BACKPACK, desc="查看你的背包")
     @player_required
     async def handle_backpack(self, event: AstrMessageEvent, player=None):
         async for r in self.shop_handler.handle_backpack(event, player):  # type: ignore
             yield r
 
-    @filter.command(config.CMD_BUY, "购买物品")
+    @filter.command(config.CMD_BUY, desc="购买物品")
     @player_required
     async def handle_buy(
         self,
@@ -140,7 +140,7 @@ class XiuXianPlugin(Star):
         async for r in self.shop_handler.handle_buy(event, item_name, quantity, player):  # type: ignore
             yield r
 
-    @filter.command(config.CMD_USE_ITEM, "使用背包中的物品")
+    @filter.command(config.CMD_USE_ITEM, desc="使用背包中的物品")
     @player_required
     async def handle_use(
         self,
@@ -153,7 +153,7 @@ class XiuXianPlugin(Star):
             yield r
 
     # Sect Commands
-    @filter.command(config.CMD_CREATE_SECT, "创建你的宗门")
+    @filter.command(config.CMD_CREATE_SECT, desc="创建你的宗门")
     @player_required
     async def handle_create_sect(
         self, event: AstrMessageEvent, sect_name: str, player=None
@@ -161,7 +161,7 @@ class XiuXianPlugin(Star):
         async for r in self.sect_handler.handle_create_sect(event, sect_name, player):  # type: ignore
             yield r
 
-    @filter.command(config.CMD_JOIN_SECT, "加入一个宗门")
+    @filter.command(config.CMD_JOIN_SECT, desc="加入一个宗门")
     @player_required
     async def handle_join_sect(
         self, event: AstrMessageEvent, sect_name: str, player=None
@@ -169,31 +169,31 @@ class XiuXianPlugin(Star):
         async for r in self.sect_handler.handle_join_sect(event, sect_name, player):  # type: ignore
             yield r
 
-    @filter.command(config.CMD_LEAVE_SECT, "退出当前宗门")
+    @filter.command(config.CMD_LEAVE_SECT, desc="退出当前宗门")
     @player_required
     async def handle_leave_sect(self, event: AstrMessageEvent, player=None):
         async for r in self.sect_handler.handle_leave_sect(event, player):  # type: ignore
             yield r
 
-    @filter.command(config.CMD_MY_SECT, "查看我的宗门信息")
+    @filter.command(config.CMD_MY_SECT, desc="查看我的宗门信息")
     @player_required
     async def handle_my_sect(self, event: AstrMessageEvent, player=None):
         async for r in self.sect_handler.handle_my_sect(event, player):  # type: ignore
             yield r
 
     # Combat Commands
-    @filter.command(config.CMD_SPAR, "与其他玩家切磋")
+    @filter.command(config.CMD_SPAR, desc="与其他玩家切磋")
     @player_required
     async def handle_spar(self, event: AstrMessageEvent, player=None):
         async for r in self.combat_handler.handle_spar(event, player):  # type: ignore
             yield r
 
-    @filter.command(config.CMD_BOSS_LIST, "查看当前所有世界Boss")
+    @filter.command(config.CMD_BOSS_LIST, desc="查看当前所有世界Boss")
     async def handle_boss_list(self, event: AstrMessageEvent):
         async for r in self.combat_handler.handle_boss_list(event):
             yield r
 
-    @filter.command(config.CMD_FIGHT_BOSS, "讨伐指定ID的世界Boss")
+    @filter.command(config.CMD_FIGHT_BOSS, desc="讨伐指定ID的世界Boss")
     @player_required
     async def handle_fight_boss(
         self, event: AstrMessageEvent, boss_id: str, player=None
@@ -208,19 +208,19 @@ class XiuXianPlugin(Star):
             yield r
 
     # Realm Commands
-    @filter.command(config.CMD_ENTER_REALM, "根据当前境界，探索一个随机秘境")
+    @filter.command(config.CMD_ENTER_REALM, desc="根据当前境界，探索一个随机秘境")
     @player_required
     async def handle_enter_realm(self, event: AstrMessageEvent, player=None):
         async for r in self.realm_handler.handle_enter_realm(event, player):  # type: ignore
             yield r
 
-    @filter.command(config.CMD_REALM_ADVANCE, "在秘境中前进")
+    @filter.command(config.CMD_REALM_ADVANCE, desc="在秘境中前进")
     @player_required
     async def handle_realm_advance(self, event: AstrMessageEvent, player=None):
         async for r in self.realm_handler.handle_realm_advance(event, player):  # type: ignore
             yield r
 
-    @filter.command(config.CMD_LEAVE_REALM, "离开当前秘境")
+    @filter.command(config.CMD_LEAVE_REALM, desc="离开当前秘境")
     @player_required
     async def handle_leave_realm(self, event: AstrMessageEvent, player=None):
         async for r in self.realm_handler.handle_leave_realm(event, player):  # type: ignore
